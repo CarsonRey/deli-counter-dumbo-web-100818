c6a48424b4e4a290e5ef60f1264dfd27c01887c0
Carson Rey <c.rey494@gmail.com>
2018-09-17 19:02:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,6 @@ def line(katz_deli)
   current_line = []
   katz_deli.each_with_index do |name, index|
       current_line << " #{index + "1".to_i}. #{name}"
-    
     end
   if katz_deli.length <= 0 
     puts "The line is currently empty."
